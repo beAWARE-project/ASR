6eceec171471278d2a50a897d8d31d7116e7a1c3
michem32 <29971931+michem32@users.noreply.github.com>
2018-05-11 12:08:25 +0300

    Update Transcriber.java

--- src/main/java/com/example/asr/Transcriber.java
+++ src/main/java/com/example/asr/Transcriber.java
@@ -69,7 +69,7 @@ public static String transcribe(String fileURL, String language) throws IOExcept
     	// //configuration.setAcousticModelPath("file:acoustic_models/Spanish/cmu_spanish/cmusphinx-es-5.2/model_parameters/voxforge_es_sphinx.cd_ptm_4000");
     	configuration.setDictionaryPath("file:acoustic_models/Spanish/cmu_spanish/es.dict");
     	// //configuration.setDictionaryPath("file:acoustic_models/Spanish/cmu_spanish/cmusphinx-es-5.2/etc/voxforge_es_sphinx");//words missing from the dictionary
-    	configuration.setLanguageModelPath("file:acoustic_models/Spanish/cmu_spanish/es-20k.lm");
+    	configuration.setLanguageModelPath("file:acoustic_models/Spanish/cmu_spanish/es-20k.lm.bin");
         // //alternativelly 
     	configuration.setAcousticModelPath("file:acoustic_models/Spanish/spanish 2/voxforge-es-0.2/model_parameters/voxforge_es_sphinx.cd_ptm_3000");
     	//configuration.setDictionaryPath("file:acoustic_models/Spanish/spanish 2/voxforge-es-0.2/etc/voxforge_es_sphinx.dic");
