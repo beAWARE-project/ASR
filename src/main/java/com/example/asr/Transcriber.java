8b7c998fe803c15a01c213cf0db23b6f2debad23
michem32 <29971931+michem32@users.noreply.github.com>
2018-10-30 13:41:52 +0200

    Update Transcriber.java

--- src/main/java/com/example/asr/Transcriber.java
+++ src/main/java/com/example/asr/Transcriber.java
@@ -77,7 +77,8 @@ public static String transcribe(String fileURL, String language) throws IOExcept
 
         break;
     case "el-GR"://Greek 
-        configuration.setAcousticModelPath("file:acoustic_models/Greek/cmusphinx-el-gr-5.2/el-gr.cd_cont_5000");
+        //configuration.setAcousticModelPath("file:acoustic_models/Greek/cmusphinx-el-gr-5.2/el-gr.cd_cont_5000");
+	configuration.setAcousticModelPath("file:acoustic_models/Greek/cmusphinx-el-gr-5.2/el-gr.cd_cont_5000_adapted2");
         configuration.setDictionaryPath("file:acoustic_models/Greek/cmusphinx-el-gr-5.2/el-gr.dic");
         configuration.setLanguageModelPath("file:acoustic_models/Greek/cmusphinx-el-gr-5.2/el-gr.lm.bin");
         //configuration.setLanguageModelPath("file:acoustic_models/Greek/cmusphinx-el-gr-5.2/el-gr.lm");
