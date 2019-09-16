63ab2cf161d6a9a00be9c37354fa0058f06ce542
michem32 <29971931+michem32@users.noreply.github.com>
2019-09-16 15:50:43 +0300

    Update Transcriber.java

--- src/main/java/com/example/asr/Transcriber.java
+++ src/main/java/com/example/asr/Transcriber.java
@@ -53,6 +53,7 @@ public static String transcribe(String fileURL, String language) throws IOExcept
 	else {//ends function
 		
 		
+	RawDataStore.storeFile(targetName,"test_audio.wav");	
 		
 		
 	// 2. Load ASR configuration 
